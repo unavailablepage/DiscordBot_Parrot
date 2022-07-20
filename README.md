348af8fbd751e1ddd640657ca30e18c9d441d935
Nicole Tendencia <tendencia.nicole@gmail.com>
2022-07-20 21:42:16 +0000

    Added slash commands

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1>Speak Bot</h1>
+<h1>Parrot</h1>
 
 <p>This is a Discord text-to-speech bot.</p>
 
@@ -8,4 +8,4 @@
 
 <p>Many users use Discord's voice channels to communicate with their friends. Some users are unable to use their mics, forcing them to use a text channel.</p>
 
-<p>The other users in the call may not check the text channel often enough. In an effort to be more inclusive for users unable to use their mic, this bot can be used to read the users' messages aloud.</p>
+<p>The other users in the call may not check the text channel often enough. In an effort to be more inclusive for users unable to use their mic, Parrot can be used to read the users' messages aloud.</p>
--- index.js
+++ index.js
@@ -1,13 +1,91 @@
-const { Client, Intents } = require('discord.js')
+const DiscordJS = require("discord.js")
+const { Client, Intents } = DiscordJS
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] })
 const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice')
 const googleTTS = require('google-tts-api')
 
 // PREFIX
-const prefix = "/"
+const prefix = "?"
 
 client.on("ready", ()=> {
   console.log('Logged on as ' + client.user.tag)
+
+  // STATUS
+  client.user.setActivity("you", { type: "WATCHING" });
+
+  const guildID = process.env.guildID
+  const guild = client.guilds.cache.get(guildID)
+  let commands
+
+  if (guild) { commands = guild.commands } 
+  else { commands = client.application?.commands }
+
+  // JOIN
+  commands?.create({
+    name: "join",
+    description: "Bot will join the voice channel that the user is in.",
+  })
+  // SPEAK
+  commands?.create({
+    name: "speak",
+    description: "The bot will announce your message within the voice channel.",
+    options: [
+      {
+        name: "message",
+        description: "The message you want announced.",
+        required: true,
+        type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING,
+      }
+    ],
+  })
+  
+})
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) { return }
+
+  const { commandName, options } = interaction
+
+  // JOIN
+  if (commandName === "join") {
+    const connection = joinVoiceChannel({
+            channelId: interaction.member.voice.channel.id,
+            guildId: interaction.guild.id,
+            adapterCreator: interaction.guild.voiceAdapterCreator
+        })
+    interaction.reply ({
+      content: "Connection successful.",
+      ephemeral: true,
+    })
+  }
+  // SPEAK
+  else if (commandName === "speak") {
+    const message = options.getString("message")
+
+    // Join VC
+    const connection = joinVoiceChannel({
+            channelId: interaction.member.voice.channel.id,
+            guildId: interaction.guild.id,
+            adapterCreator: interaction.guild.voiceAdapterCreator
+        })
+
+    // Audio
+    const url = googleTTS.getAudioUrl(message, {
+      lang: 'en',
+      slow: false,
+      host: 'https://translate.google.com',
+    });
+
+    const player = createAudioPlayer();
+    const resource = createAudioResource(url, { inlineVolume: true })
+    connection.subscribe(player); 
+    player.play(resource)
+  }
+  
+  interaction.reply ({
+      content: "Your message: " + options.getString("message"),
+      ephemeral: true,
+    })
 })
 
 client.on("messageCreate", msg => {
@@ -17,17 +95,7 @@ client.on("messageCreate", msg => {
 	const command = args.shift().toLowerCase()
   const message = args.join(" ")
 
-  // PING
-  if (command == "ping") { msg.reply("pong") }
-  // PARROT
-  else if (command == "parrot") { msg.reply(message) }
-  /*
-    The bot will join the voice channel the user is in,
-    and it will speak the message the user provided.
-
-    Example: /s Hello world!
-  */
-  else if (command == "s") {
+  if (command == "s") {
     const connection = joinVoiceChannel({
             channelId: msg.member.voice.channel.id,
             guildId: msg.guild.id,
